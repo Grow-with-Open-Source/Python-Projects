6cf60fcadffc1e7bb8bb08bb91d0a73f180466a8
Adegbemigun Adedayo <117754792+adedayoprcs@users.noreply.github.com>
2023-10-26 18:33:20 +0100

    destination path for files

--- File_Organizer/file-organizer.py
+++ File_Organizer/file-organizer.py
@@ -1,18 +1,36 @@
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import shutil
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         if not event.is_directory:
             file_path = event.src_path
             file_name = os.path.basename(file_path)
-            file_extension = os.path.splitext(file_name)[1].lower()  # Convert to lowercase
+            file_extension = os.path.splitext(file_name)[1].lower()
+            jpg_destination = r"C:\Users\Precious pc\Pictures\download images"
+            zip_destination = r"C:\Users\Precious pc\Documents\Zip files"
+            png_destination = r"C:\Users\Precious pc\Documents\png_files"
+            psd_destination = r"C:\Users\Precious pc\Documents\psd_destination"
+            pdf_files = r"C:\Users\Precious pc\Documents\pdf_files"
+            other_files = r"C:\Users\Precious pc\Documents\other_files"
+
 
             if file_extension == ".jpg":
-                print(f"File extension: {file_extension}")
+                shutil.move(file_path , jpg_destination)
+            elif file_extension == ".zip":
+                shutil.move(file_path, zip_destination)
+            elif file_extension == ".png":
+                shutil.move(file_path, png_destination)
+            elif file_extension == ".psd":
+                print(file_extension)
+                shutil.move(file_path, psd_destination)
+            elif file_extension == ".pdf":
+                shutil.move(file_path, pdf_files)
             else:
-                print("None")
+                shutil.move(file_path, other_files)
+
 
 if __name__ == "__main__":
     folder_to_watch = r"C:\Users\Precious pc\Documents\file-organizer"  # Replace with the directory you want to monitor
