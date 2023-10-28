45914faf53824c9e31a547ea62351a750cc4ca3a
Ravi <koraravindra6@gmail.com>
2023-10-28 17:39:27 +0530

    Updated the code

--- QRCODE-Generator/QRCODE-Generator.py
+++ QRCODE-Generator/QRCODE-Generator.py
@@ -21,7 +21,7 @@ def generate_qr_code():
 
     # Display the QR code image in the GUI
     qr_image = Image.open("url_qrcode.png")
-    qr_image = qr_image.resize((200, 200), Image.LANCZOS)
+    qr_image = qr_image.resize((400, 400), Image.LANCZOS)  # Larger size
     qr_photo = ImageTk.PhotoImage(qr_image)
     qr_label.config(image=qr_photo)
     qr_label.image = qr_photo
@@ -37,20 +37,23 @@ def open_url(event):
 window = tk.Tk()
 window.title("QR Code Generator")
 
-# Create and place a label for the URL input
-url_label = tk.Label(window, text="Enter the URL:")
-url_label.pack()
+# Increase the window size
+window.geometry("600x600")
 
-url_entry = tk.Entry(window)
-url_entry.pack()
+# Create and place a label for the URL input
+url_label = tk.Label(window, text="Enter the URL:", font=("Arial", 14))
+url_label.pack(pady=10)  # Adds vertical spacing
+
+url_entry = tk.Entry(window, font=("Arial", 14))
+url_entry.pack(pady=10)  # Adds vertical spacing
 
 # Create and place a button to generate the QR code
-generate_button = tk.Button(window, text="Generate QR Code", command=generate_qr_code)
-generate_button.pack()
+generate_button = tk.Button(window, text="Generate QR Code", command=generate_qr_code, font=("Arial", 14))
+generate_button.pack(pady=10)  # Adds vertical spacing
 
 # Create a label to display the generated QR code
 qr_label = tk.Label(window)
-qr_label.pack()
+qr_label.pack(pady=10)  # Adds vertical spacing
 
 # Bind a click event to the QR code label
 qr_label.bind("<Button-1>", open_url)
