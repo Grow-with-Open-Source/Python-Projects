e706f1475c1f65be839a7a11a22f6fd43a988f31
Adegbemigun Adedayo <117754792+adedayoprcs@users.noreply.github.com>
2023-10-29 00:51:48 +0100

    added descriptive texts

--- File_Organizer/file-organizer.py
+++ File_Organizer/file-organizer.py
@@ -5,32 +5,43 @@ import shutil
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
-        if not event.is_directory:
-            file_path = event.src_path
-            file_name = os.path.basename(file_path)
-            file_extension = os.path.splitext(file_name)[1].lower()
-            jpg_destination = r"C:\Users\Precious pc\Pictures\download images"
-            zip_destination = r"C:\Users\Precious pc\Documents\Zip files"
-            png_destination = r"C:\Users\Precious pc\Documents\png_files"
-            psd_destination = r"C:\Users\Precious pc\Documents\psd_destination"
-            pdf_files = r"C:\Users\Precious pc\Documents\pdf_files"
-            other_files = r"C:\Users\Precious pc\Documents\other_files"
+        try:
+            if not event.is_directory:
+                file_path = event.src_path
+                file_name = os.path.basename(file_path)
+                file_extension = os.path.splitext(file_name)[1].lower()
 
 
-            if file_extension == ".jpg":
-                shutil.move(file_path , jpg_destination)
-            elif file_extension == ".zip":
-                shutil.move(file_path, zip_destination)
-            elif file_extension == ".png":
-                shutil.move(file_path, png_destination)
-            elif file_extension == ".psd":
-                print(file_extension)
-                shutil.move(file_path, psd_destination)
-            elif file_extension == ".pdf":
-                shutil.move(file_path, pdf_files)
-            else:
-                shutil.move(file_path, other_files)
+            try:
+                # A dictionary takes file extensions & folder to move each formats to. 
+                destination_mapping = {
+                    #Key = Format/extension to handle : Value = Folder to move file format. 
+                    ".jpg": r"C:\Users\Precious pc\Pictures\download imag",
+                    ".zip": r"C:\Users\Precious pc\Documents\Zip files",
+                    ".png": r"C:\Users\Precious pc\Documents\png_files",
+                    ".psd": r"C:\Users\Precious pc\Documents\psd_destination",
+                    ".pdf": r"C:\Users\Precious pc\Documents\pdf_files",
+                }
+            except ValueError:
+                print("folder not found")
 
+                # Default destination for unknown extensions
+                other_files = r"C:\Users\Precious pc\Documents\other_files"
+
+                # Get the destination directory for the file extension or use the default
+                destination = destination_mapping.get(file_extension, other_files)
+
+                # Move the file to the determined destination
+                
+                # Check if file already exists in destination & delete it. 
+                if os.path.exists(os.path.join(destination, file_name)):
+                                print(f"File {file_name} already exists in the destination. Deleting it.")
+                                os.remove(os.path.join(destination, file_name))
+
+                            # Move the file to the determined destination
+                shutil.move(file_path, destination)
+        except ValueError:
+            print("an error occur!")
 
 if __name__ == "__main__":
     folder_to_watch = r"C:\Users\Precious pc\Documents\file-organizer"  # Replace with the directory you want to monitor
@@ -44,4 +55,15 @@ if __name__ == "__main__":
             pass
     except KeyboardInterrupt:
         observer.stop()
-    observer.join()
+    observer.join()               
+
+
+
+
+
+
+          
+
+            
+            
+
