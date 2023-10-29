76a0fa3ae2e1c125849254cbf40c0e286d0ab8f2
Adegbemigun Adedayo <117754792+adedayoprcs@users.noreply.github.com>
2023-10-29 01:10:56 +0100

    created a README file

--- File_Organizer/file-organizer.py
+++ File_Organizer/file-organizer.py
@@ -5,25 +5,23 @@ import shutil
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
-        try:
-            if not event.is_directory:
+        if not event.is_directory:
                 file_path = event.src_path
                 file_name = os.path.basename(file_path)
                 file_extension = os.path.splitext(file_name)[1].lower()
 
 
-            try:
+         
                 # A dictionary takes file extensions & folder to move each formats to. 
                 destination_mapping = {
                     #Key = Format/extension to handle : Value = Folder to move file format. 
-                    ".jpg": r"C:\Users\Precious pc\Pictures\download imag",
+                   
                     ".zip": r"C:\Users\Precious pc\Documents\Zip files",
                     ".png": r"C:\Users\Precious pc\Documents\png_files",
                     ".psd": r"C:\Users\Precious pc\Documents\psd_destination",
                     ".pdf": r"C:\Users\Precious pc\Documents\pdf_files",
                 }
-            except ValueError:
-                print("folder not found")
+      
 
                 # Default destination for unknown extensions
                 other_files = r"C:\Users\Precious pc\Documents\other_files"
@@ -40,11 +38,9 @@ class MyHandler(FileSystemEventHandler):
 
                             # Move the file to the determined destination
                 shutil.move(file_path, destination)
-        except ValueError:
-            print("an error occur!")
-
+    
 if __name__ == "__main__":
-    folder_to_watch = r"C:\Users\Precious pc\Documents\file-organizer"  # Replace with the directory you want to monitor
+    folder_to_watch = r"C:\Users\Precious pc\Documents\monitor"  # Replace with the directory you want to monitor
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path=folder_to_watch, recursive=False)  # Set recursive to True if you want to monitor subdirectories
@@ -55,7 +51,11 @@ if __name__ == "__main__":
             pass
     except KeyboardInterrupt:
         observer.stop()
-    observer.join()               
+    observer.join()
+
+
+      
+                
 
 
 
