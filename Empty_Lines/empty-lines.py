67a53d8d83915c16c9e6e003d35ed89a76ab43db
Adegbemigun Adedayo <117754792+adedayoprcs@users.noreply.github.com>
2023-10-30 18:35:06 +0100

    I created sys argv

--- Empty_Lines/empty-lines.py
+++ Empty_Lines/empty-lines.py
@@ -4,6 +4,12 @@ try:
     input_filename = sys.argv[1]
     output_filename = sys.argv[2]
 
+    with open(input_filename) as File:
+        lines = []
+        for line in File:
+            if not line.space():
+                lines.append(line)
+
     
     with open(output_filename, "w") as output:
         for line in lines:
