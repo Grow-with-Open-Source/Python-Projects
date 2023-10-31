21da0bb9ed21fc38947a7856e7d4f779b224e0fe
Adegbemigun Adedayo <117754792+adedayoprcs@users.noreply.github.com>
2023-10-31 10:04:39 +0000

    Removed all empty lines

--- Remove-Empty-Lines/remove-empty-lines.py
+++ Remove-Empty-Lines/remove-empty-lines.py
@@ -1,21 +1,14 @@
 import sys
-
-# Check if the correct number of arguments is provided
 if len(sys.argv) != 3:
     print("Usage: python remove_empty_lines.py input_file output_file")
     sys.exit(1)
-
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-
 try:
-    # Open the input file for reading and the output file for writing
     with open(input_file, "r") as in_file, open(output_file, "w") as out_file:
         for line in in_file:
-            # Check if the line is not empty (contains at least one non-space character)
             if line.strip():
                 out_file.write(line)
-
     print(f"Empty lines removed and saved to {output_file}")
 except FileNotFoundError:
     print("File not found. Please check the file paths.")
