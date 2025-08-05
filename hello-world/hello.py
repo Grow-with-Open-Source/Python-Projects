5137e1bb389b619b174bfd384a8556cf434a72c2
Shailaja Poojari <shailaja.s.poojary091@gmail.com>
2025-08-05 12:47:28 +0530

    Add Age Calculator Python project

---
+++ hello-world/hello.py
@@ -0,0 +1,7 @@
+def say_hello(name):
+    return f"Hello, {name}! Welcome to the world of open source."
+
+# Example usage
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(say_hello(user_name))
