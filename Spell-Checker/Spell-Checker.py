ad5ae9a1e7fabc344f212ca049d7ded85a85b075
Princee <princec14552@gamil.com>
2026-04-10 12:15:17 +0545

    Add input validation and success messaging

--- Spell-Checker/Spell-Checker.py
+++ Spell-Checker/Spell-Checker.py
@@ -3,8 +3,14 @@ from textblob import TextBlob
 
 def check_spelling():
     input_text = input_entry.get()
-    corrected_text = TextBlob(input_text).correct()
-    result_label.config(text="Corrected text: " + str(corrected_text))
+    if not input_text:
+        result_label.config(text="TextBox cannot be empty!", fg="red")
+        return
+    corrected_text = str(TextBlob(input_text).correct())
+    if input_text!=corrected_text:
+        result_label.config(text="Corrected text: " + corrected_text, fg="black")
+        return
+    result_label.config(text=f"{input_text} is Correct!", fg="green")
 
 def reset():
     input_entry.delete(0, tk.END)
